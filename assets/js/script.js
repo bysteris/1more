6df11ac45380bc22651ef35279a62b57ac8f8aca
Valeria Zolotareva <panda.lera@mail.ru>
2022-09-19 19:02:24 +0500

    главный экран есть, на этапе эволюции

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
+  //swiper на главном экране
+  var mySwiper = new Swiper(".swiper-container", {
+    direction: "vertical",
+    loop: false,
+    pagination: ".swiper-pagination",
+    grabCursor: false,
+    speed: 1000,
+    paginationClickable: true,
+    parallax: true,
+    autoplay: false,
+    effect: "slide",
+    //mousewheelControl: 1,
+    mousewheel: {
+      invert: false,
+      releaseOnEdges: true,
+    },
+  });
 
-    new fullpage('#fullpage', {
-        
-    });
 
 });
-
-
