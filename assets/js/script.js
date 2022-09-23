3c1225656249f580865a344517c84eb848d0adf5
Valeria Zolotareva <panda.lera@mail.ru>
2022-09-23 16:26:40 +0500

    3 экран функционирует

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,6 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   });
 
-
   const bcg = document.querySelector(".evolution-line .move");
 
   window.addEventListener("scroll", () => {
@@ -37,11 +36,13 @@ document.addEventListener("DOMContentLoaded", function () {
   // Инит AOSanimation
   AOS.init();
 
+  gsap.registerPlugin(ScrollTrigger);
+
   gsap.to(".banner-radius", {
     scrollTrigger: {
       trigger: ".banner-radius",
       start: "25% 100%",
-      end: "100% 95%",
+      end: "100% 100%",
       scrub: 1,
     },
     scale: 1,
@@ -49,6 +50,51 @@ document.addEventListener("DOMContentLoaded", function () {
     borderTopRightRadius: 0,
     duration: 2,
   });
+
+  gsap.from(".banner-title", {
+    scrollTrigger: {
+      trigger: ".banner-title",
+      start: "-100% 100%",
+      end: "0% 100%",
+      scrub: 1,
+    },
+    yPercent: 100,
+    ease: "SlowMo",
+    stagger: 0.2,
+    duration: 2,
+  });
+
+  gsap.from(".banner-description_wrapper", {
+    scrollTrigger: {
+      trigger: ".banner-description_wrapper",
+      start: "100% 100%",
+      end: "200% 100%",
+      scrub: 1,
+      //markers: true
+    },
+    xPercent: -10,
+    opacity: 0,
+    ease: "SlowMo",
+    stagger: 0.2,
+    duration: 2,
+  });
+
   
+
+  let sections = gsap.utils.toArray(".banner_horizontal-container .page");
+
+  gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "SlowMo",
+    scrollTrigger: {
+      trigger: ".banner-wrapper",
+      pin: true,
+      start: "top",
+      //markers: true,
+      scrub: 1,
+      end: () => "+=" + document.querySelector(".banner_horizontal-container").offsetWidth / 4,
+    },
+  });
+
 
 });
